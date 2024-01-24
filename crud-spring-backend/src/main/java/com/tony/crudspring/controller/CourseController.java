49936fddd26f7a3ee8e7bbb8588b3afd8c165f67
tonyfilho <oficinadosdrones@gmail.com>
2024-01-24 11:28:22 +0000

    Re factory and Service layer

--- crud-spring-backend/src/main/java/com/tony/crudspring/controller/CourseController.java
+++ crud-spring-backend/src/main/java/com/tony/crudspring/controller/CourseController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tony.crudspring.model.Course;
 import com.tony.crudspring.repository.CourseRepository;
+import com.tony.crudspring.service.CourseService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -24,58 +25,59 @@ import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 
 //Obs: @Valid serve para as validações das ENTIDADES e não as Locais.
-@Validated // @Validated serve p as validações Locais Ex: locais @NotNull @Positive funcione somente nos parametros dos mehodos e não as da entidade
+@Validated // @Validated serve p as validações Locais Ex: locais @NotNull @Positive
+           // funcione somente nos parametros dos mehodos e não as da entidade
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor
+// @AllArgsConstructor
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+    private final CourseService courseService;
+
+    /** Criamos o construtor ao invez de usar o lombok */
+    public CourseController(CourseService courseService) {
+
+        this.courseService = courseService;
+    }
 
     @GetMapping
     public List<Course> list() {
-        return courseRepository.findAll();
+        return courseService.list();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Course> findById(@PathVariable("id") @NotNull @Positive Long id) {
-        return courseRepository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound))
+        return courseService.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<Course> create(@RequestBody @Valid Course course) {
-        courseRepository.save(course);
+        courseService.create(course);
         return ResponseEntity.status(HttpStatus.CREATED).body(course);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
 
-        return courseRepository.findById(id).map(recordFound -> {
-            recordFound.setName(course.getName());
-            recordFound.setCategory(course.getCategory());
-            Course updateCourse = courseRepository.save(recordFound);
-            return ResponseEntity.ok().body(updateCourse);
+        return courseService.update(id, course).map(recordFound -> {
+            return ResponseEntity.ok().body(recordFound);
         })
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Course> delete(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return ResponseEntity.noContent().<Course>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+        if (courseService.delete(id)) {
+            return ResponseEntity.noContent().<Course>build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
     /** ex: de retornos */
     public ResponseEntity<Object> delete2(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
+        return courseService.delete2(id)
                 .map(recordFound -> {
-                    courseRepository.deleteById(id);
                     return ResponseEntity.noContent().build();
                 })
                 .orElse(ResponseEntity.notFound().build());
@@ -83,9 +85,8 @@ public class CourseController {
 
     /** ex: de retornos */
     public ResponseEntity<?> delete3(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
+        return courseService.delete2(id)
                 .map(recordFound -> {
-                    courseRepository.deleteById(id);
                     return ResponseEntity.noContent().build();
                 })
                 .orElse(ResponseEntity.notFound().build());
@@ -93,9 +94,8 @@ public class CourseController {
 
     /** ex: de retornos */
     public ResponseEntity<Void> delete4(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
+        return courseService.delete2(id)
                 .map(recordFound -> {
-                    courseRepository.deleteById(id);
                     return ResponseEntity.noContent().<Void>build();
                 })
                 .orElse(ResponseEntity.notFound().build());
