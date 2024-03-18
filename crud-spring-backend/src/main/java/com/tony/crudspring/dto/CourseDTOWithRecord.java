ec8bbe7a78c2642c4ce7e2ebfd79ca9f6373ce02
tonyfilho <oficinadosdrones@gmail.com>
2024-03-18 09:22:28 +0000

    Enums Validations, toString to Enums
    ValueOfEnumValidator and ValueOfEnumValidator.java

--- crud-spring-backend/src/main/java/com/tony/crudspring/dto/CourseDTOWithRecord.java
+++ crud-spring-backend/src/main/java/com/tony/crudspring/dto/CourseDTOWithRecord.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tony.crudspring.enums.Category;
+import com.tony.crudspring.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+
 /**
  * Manteremos somente as validações e removeremos tudo que for para JPA e
  * Hibernate ligados ao DB
@@ -22,15 +24,17 @@ import jakarta.validation.constraints.Pattern;
  */
 /** Não se consegue mudar as informações,a RECORD é uma class IMUTAVEL */
 /**E não é usado mais a palavra GET, se aplica diretamente o nome das PROPRIEDADES */
-/**A Record Não extend class, mas pode implementar interfaces */
+/** A Record Não extend class, mas pode implementar interfaces */
 
 public record CourseDTOWithRecord(
-    @JsonProperty("_id") Long id, 
-    @NotBlank @NotNull @Length(min = 2, max = 100) String name, 
-    @NotNull @Length(max = 10) @Pattern(regexp = "back-end|front-end") String category,
-    @NotNull @NotEmpty @Valid List<LessonDTOWithRecord> lessons
-    ) {
+        /*
+         * @NotNull @Length(max = 10) @Pattern(regexp = "back-end|front-end") String
+         * category //passaremos a usar a interface e a class que converteString em
+         * Enums
+         */
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 2, max = 100) String name,
+        @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
+        @NotNull @NotEmpty @Valid List<LessonDTOWithRecord> lessons) {
 
-
-    
 }
